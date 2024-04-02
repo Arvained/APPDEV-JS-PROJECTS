6653c337cd6e11f4bbf5034c11de6acf66b848d6
Arvin <159758877+Arvained@users.noreply.github.com>
2024-04-02 16:11:51 +0800

    Delete src directory

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function footer(){
-    return(
-        <footer>
-            <p>&copy; {new Date().getFullYear()}
-            &nbsp;
-             My Attendance Monitoring Website || Written by: Padilla</p>
-        </footer>
-    )
-}
