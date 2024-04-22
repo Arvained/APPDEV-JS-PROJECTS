eb2d94bb65d09e674b78586ddcfccc859d10fe2d
Arvin <159758877+Arvained@users.noreply.github.com>
2024-04-22 15:20:15 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import FavoriteGames from "./FavoriteGames"
+export default function App(){
+    return(
+      <>
+        <Header />
+        <FavoriteGames/>
+        <Footer />
+        
+      </>
+    )
+}
